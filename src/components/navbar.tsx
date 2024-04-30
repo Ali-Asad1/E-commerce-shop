03873e829ef611404eae30e2cffa7911ab01d129
Ali Asad <aliasad.3128@gmail.com>
2024-04-30 15:53:01 +0330

    feat(ui): Implement `Navbar` component with dynamic data fetching

---
+++ src/components/navbar.tsx
@@ -0,0 +1,23 @@
+import { getCategories } from "@/actions/get-categories";
+import { getStore } from "@/actions/get-store";
+import Link from "next/link";
+
+import MainNav from "./main-nav";
+
+const Navbar = async () => {
+  const categories = await getCategories();
+  const store = await getStore();
+  return (
+    <div className="border-b">
+      <div className="container">
+        <div className="flex h-16 items-center">
+          <Link href="/" className="flex gap-x-2 lg:ml-0">
+            <p className="font-poppins text-xl font-bold">{store.name}</p>
+          </Link>
+          <MainNav data={categories} />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
