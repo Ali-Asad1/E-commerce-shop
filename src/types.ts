a611d0d273d699a25bff417692a94aea89331fc5
Ali Asad <aliasad.3128@gmail.com>
2024-05-01 22:45:01 +0330

    feat(types): Update types to use Type suffix for clarity and Add `ProductType`

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,44 @@
-export type Billboard = {
+export type BillboardType = {
   id: string;
   label: string;
   imageUrl: string;
 };
 
-export type Category = {
+export type CategoryType = {
   id: string;
   name: string;
-  billboard: Billboard;
+  billboard: BillboardType;
 };
 
-export type Store = {
+export type StoreType = {
   id: string;
   name: string;
   userId: string;
+};
+
+export type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  isFeatured: boolean;
+  category: CategoryType;
+  size: SizeType;
+  images: ImageType[];
+  color: ColorType;
+};
+
+export type ImageType = {
+  id: string;
+  url: string;
+};
+
+export type ColorType = {
+  id: string;
+  name: string;
+  value: string;
+};
+export type SizeType = {
+  id: string;
+  name: string;
+  value: string;
 };
