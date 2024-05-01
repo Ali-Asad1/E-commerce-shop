f7f9e217d70956748681aa509dc6a534a4a1bc3c
Ali Asad <aliasad.3128@gmail.com>
2024-05-01 22:45:58 +0330

    chore(types): Update `getCategories` function to use `CategoryType`

--- src/actions/get-categories.ts
+++ src/actions/get-categories.ts
@@ -1,8 +1,8 @@
-import { Category } from "@/types";
+import { CategoryType } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-export const getCategories = async (): Promise<Category[]> => {
+export const getCategories = async (): Promise<CategoryType[]> => {
   const response = await fetch(URL, { next: { revalidate: 10 } });
   return response.json();
 };
