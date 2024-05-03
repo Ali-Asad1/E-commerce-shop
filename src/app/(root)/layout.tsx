d0e842acd8570a479783c4ef549394ade7574303
Ali Asad <aliasad.3128@gmail.com>
2024-05-03 18:05:23 +0330

    chore(components): Remove `revalidate` from `MainLayout`

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,6 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
-export const revalidate = 5000;
-
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-full w-full flex-col">
