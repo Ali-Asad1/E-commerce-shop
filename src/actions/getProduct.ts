28ae1627df8f7d11487d17a3c5dde200928a1857
Ali Asad <aliasad.3128@gmail.com>
2024-05-04 20:26:01 +0330

    feat(api): Implement `getProduct` function

---
+++ src/actions/getProduct.ts
@@ -0,0 +1,8 @@
+import { ProductType } from "@/types";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+export const getProduct = async (id: string): Promise<ProductType> => {
+  const response = await fetch(`${URL}/${id}`);
+  return response.json();
+};
