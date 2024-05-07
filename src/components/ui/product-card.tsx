afc8e435f454a8b6a6f6ddbb9a449c70721d195d
Ali Asad <aliasad.3128@gmail.com>
2024-05-07 14:15:28 +0330

    feat(ui): Add tooltip functionality to display full product name on hover

--- src/components/ui/product-card.tsx
+++ src/components/ui/product-card.tsx
@@ -7,6 +7,7 @@ import { ProductType } from "@/types";
 import { formatter } from "@/utils/utils";
 
 import { Button } from "./button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip";
 
 interface ProductCardProps {
   data: ProductType;
@@ -23,23 +24,31 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
             placeholder="blur"
             blurDataURL={data.images[0].url}
             fill
+            loading="lazy"
             className="aspect-square rounded-lg object-cover"
           />
           <div className="absolute bottom-5 w-full opacity-0 transition group-hover:opacity-100">
             <div className="flex items-center justify-center gap-x-6">
-              <Button variant="secondary" size="icon">
+              <Button variant="outline" size="icon">
                 <Expand size={20} />
               </Button>
-              <Button variant="secondary" size="icon">
+              <Button variant="outline" size="icon">
                 <ShoppingCart size={20} />
               </Button>
             </div>
           </div>
         </div>
-        <div>
-          <p className="text-lg font-semibold">{data.name}</p>
-          <p className="text-sm text-muted-foreground">{data.category.name}</p>
-        </div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <p className="line-clamp-1 text-lg font-semibold">{data.name}</p>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{data.name}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <p className="text-sm text-muted-foreground">{data.category.name}</p>
         <div className="flex items-center justify-between">
           <p className="font-semibold">{formatter.format(data.price)}</p>
         </div>
