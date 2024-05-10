ee4014ab0113ab93174e058182ac01c2d6ce1b03
Ali Asad <aliasad.3128@gmail.com>
2024-05-11 01:26:18 +0330

    chore(ui): Update `NavbarActions` component to dynamically display cart item count

--- src/components/navbar-actions.tsx
+++ src/components/navbar-actions.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { ShoppingBag } from "lucide-react";
+
+import { useCart } from "@/hooks/use-cart-store";
 
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 
 const NavbarActions = () => {
+  const count = useCart((state) => state.items.length);
+
   return (
     <div className="ml-auto flex items-center gap-x-2">
       <div className="relative">
@@ -11,7 +17,7 @@ const NavbarActions = () => {
           <ShoppingBag className="h-5 w-5" />
         </Button>
         <Badge variant="destructive" className="pointer-events-none absolute -bottom-1.5 -left-1.5 select-none px-1.5">
-          12
+          {count}
         </Badge>
       </div>
     </div>
