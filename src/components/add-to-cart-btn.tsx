754b13ac9e89ab9e269e1e72a10df0e3cf0c1da3
Ali Asad <aliasad.3128@gmail.com>
2024-05-11 00:45:32 +0330

    feat(ui): Update AddToCartBtn to use store for managing cart state

--- src/components/add-to-cart-btn.tsx
+++ src/components/add-to-cart-btn.tsx
@@ -1,4 +1,10 @@
-import { ShoppingBasket } from "lucide-react";
+"use client";
+
+import { ShoppingBasket, ShoppingCart } from "lucide-react";
+
+import { ProductType } from "@/types";
+
+import { useCart } from "@/hooks/use-cart-store";
 
 import { cn } from "@/utils/utils";
 
@@ -6,13 +12,29 @@ import { Button } from "@/components/ui/button";
 
 interface AddToCartBtnProps {
   className?: string;
+  iconButton?: boolean;
+  data: ProductType;
 }
 
-const AddToCartBtn: React.FC<AddToCartBtnProps> = ({ className }) => {
-  return (
-    <Button className={cn("flex items-center gap-x-2 font-poppins", className)}>
+const AddToCartBtn: React.FC<AddToCartBtnProps> = ({ className, iconButton, data }) => {
+  const { addItem } = useCart();
+
+  const addToCartHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    addItem(data);
+  };
+
+  const element = iconButton ? (
+    <Button variant="outline" size="icon" onClick={addToCartHandler}>
+      <ShoppingCart size={20} />
+    </Button>
+  ) : (
+    <Button className={cn("flex items-center gap-x-2 font-poppins", className)} onClick={addToCartHandler}>
       Add To Cart <ShoppingBasket size={20} strokeWidth={2} />
     </Button>
   );
+
+  return element;
 };
 export default AddToCartBtn;
