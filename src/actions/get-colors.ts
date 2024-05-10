be3d0cad0cea9ad218c9f17236a1745c18eb217d
Ali Asad <aliasad.3128@gmail.com>
2024-05-10 23:13:16 +0330

    feat(api): Remove console log from `getColors` function

--- src/actions/get-colors.ts
+++ src/actions/get-colors.ts
@@ -4,6 +4,5 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`;
 
 export const getColors = async (): Promise<ColorType[]> => {
   const response = await fetch(URL, { next: { revalidate: 10 } });
-  console.log(response.status);
   return response.json();
 };
