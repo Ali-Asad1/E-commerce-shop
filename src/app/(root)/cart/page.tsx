ab4d74187bf2a0f9c10cb454b302ef818fcc3533
Ali Asad <aliasad.3128@gmail.com>
2024-05-19 16:06:56 +0330

    refactor(page): update cart page layout and components

--- src/app/(root)/cart/page.tsx
+++ src/app/(root)/cart/page.tsx
@@ -4,9 +4,9 @@ import Summary from "./_components/summary";
 const Page = () => {
   return (
     <div className="container py-12">
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        <CartList />
-        <Summary />
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-12">
+        <CartList className="col-span-1 space-y-4 lg:col-span-2" />
+        <Summary className="col-span-1" />
       </div>
     </div>
   );
