f73628d5933c325239e2f2a3e1d08cdc658cb70c
Ali Asad <aliasad.3128@gmail.com>
2024-05-19 16:03:19 +0330

    refactor(cart): extend `CartList` with additional props

--- src/app/(root)/cart/_components/cart-list.tsx
+++ src/app/(root)/cart/_components/cart-list.tsx
@@ -2,15 +2,21 @@
 
 import { useCart } from "@/hooks/use-cart-store";
 
+import { cn } from "@/utils/utils";
+
 import NoResult from "@/components/ui/no-result";
 
 import CartItem from "./cart-item";
 
-const CartList = () => {
+interface CartListProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+const CartList: React.FC<CartListProps> = ({ className, ...props }) => {
   const items = useCart((state) => state.items);
 
   return (
-    <div className="space-y-4">
+    <div className={cn(className)} {...props}>
       {items.length ? items.map((item) => <CartItem key={`cart-${item.id}`} data={item} />) : <NoResult />}
     </div>
   );
